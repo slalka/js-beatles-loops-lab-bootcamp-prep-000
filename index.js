dab70a2f51e56ed96008cdc18bd8320e2b0975c4
stephan lalka <slalka@gmail.com>
2017-11-05 00:15:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(m,i){
   var a = []
   for(j = 0;j < m.length; j++){
-    a.push(`"${m[j]} plays ${i[j]}"`)
+    a.push(`${m[j]} plays ${i[j]}`)
   }
   console.log(a)
   return a
